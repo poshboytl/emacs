6d936961cf532edc680291af2b37ef7f6c58daa4
Terry Tai <poshboytl@gmail.com>
2014-03-09 02:20:14 +0800

    Some customization.

--- defunkt.el
+++ defunkt.el
@@ -11,11 +11,11 @@
 (load "defunkt/defuns")
 (load "defunkt/bindings")
 (load "defunkt/modes")
-(load "defunkt/theme")
 (load "defunkt/temp_files")
 (load "defunkt/github")
 (load "defunkt/git")
 (load "defunkt/todo")
+
 
 (when (file-exists-p "defunkt/private")
   (load "defunkt/private"))
